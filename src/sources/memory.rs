271faf025c810554460291d8f6816f9cb2f4faa7
Ning Sun <sunng@about.me>
2015-12-22 21:16:38 +0800

    (fix) fixed MemorySource field visibility

--- src/sources/memory.rs
+++ src/sources/memory.rs
@@ -2,7 +2,7 @@ use ::source::{Source, SourceError};
 use std::collections::BTreeMap;
 use handlebars::Handlebars;
 
-pub struct MemorySource(BTreeMap<String, String>);
+pub struct MemorySource(pub BTreeMap<String, String>);
 
 impl Source for MemorySource {
     fn load(&self, reg: &mut Handlebars) -> Result<(), SourceError> {
