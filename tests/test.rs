e99b2d31dc7589ba463ebb5ab6e7c9eac4626da5
Ning Sun <sunng@about.me>
2015-12-25 13:12:46 +0800

    (test) added test for #23

--- tests/test.rs
+++ tests/test.rs
@@ -19,3 +19,20 @@ fn test_template() {
 
     assert!(hh.get_template("index").is_some());
 }
+
+#[test]
+fn test_template2() {
+    let mut hbse = HandlebarsEngine::new2();
+    let src = Box::new(DirectorySource::new("./examples/templates", ".hbs"));
+
+    hbse.add(src);
+
+    // success of panic
+    if let Err(r) = hbse.reload() {
+        panic!("{}", r.description());
+    }
+
+    let hh = hbse.registry.read().unwrap();
+
+    assert!(hh.get_template("index").is_some());
+}
