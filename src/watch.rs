edc9b7bbfdf85afc20c63b0c05585d00bfabcbbb
Ning Sun <sunng@about.me>
2016-12-14 21:39:13 +0800

    (feat) update to notify 3.0
    Signed-off-by: Ning Sun <sunng@about.me>

--- src/watch.rs
+++ src/watch.rs
@@ -1,15 +1,16 @@
 use middleware::HandlebarsEngine;
 
-use notify::{RecommendedWatcher, Error, Watcher};
+use notify::{RecommendedWatcher, Error, Watcher, RecursiveMode};
 use std::path::Path;
+use std::time::Duration;
 use std::sync::mpsc::channel;
 use std::sync::Arc;
 use std::thread;
 
-fn _watch (p: &Path, hbs: &Arc<HandlebarsEngine>) -> Result<(), Error>{
+fn _watch(p: &Path, hbs: &Arc<HandlebarsEngine>) -> Result<(), Error> {
     let (tx, rx) = channel();
-    let mut watcher: RecommendedWatcher = try!(Watcher::new(tx));
-    try!(watcher.watch(p));
+    let mut watcher: RecommendedWatcher = try!(Watcher::new(tx, Duration::from_secs(2)));
+    try!(watcher.watch(p, RecursiveMode::Recursive));
     loop {
         let _ = rx.recv();
         info!("Template directory changed");
@@ -24,7 +25,7 @@ pub trait Watchable {
 }
 
 impl Watchable for Arc<HandlebarsEngine> {
-    fn watch (&self, path: &str) {
+    fn watch(&self, path: &str) {
         let hbs = self.clone();
         let watch_path = path.to_owned();
         thread::spawn(move || {
