afcc6625eeaefe41e6447c7730f00278b8e9a9c9
Ning Sun <sunng@protonmail.com>
2018-08-18 18:09:20 +0800

    (chore) remove unused code

--- src/sources/directory.rs
+++ src/sources/directory.rs
@@ -20,21 +20,6 @@ impl DirectorySource {
     }
 }
 
-// /// return false when file name not satisfied
-// fn filter_file(entry: &DirEntry, suffix: &OsStr) -> bool {
-//     let path = entry.path();
-
-//     // ignore hidden files, emacs buffers and files with wrong suffix
-//     !path.is_file() ||
-//     path.file_name()
-//         .map(|s| {
-//                  let ds = s.to_string_lossy();
-//                  ds.starts_with(".") || ds.starts_with("#") ||
-//                  !ds.ends_with(suffix.to_string_lossy().as_ref())
-//              })
-//         .unwrap_or(true)
-// }
-
 impl Source for DirectorySource {
     fn load(&self, reg: &mut Handlebars) -> Result<(), SourceError> {
         reg.register_templates_directory(self.suffix, &self.prefix)
